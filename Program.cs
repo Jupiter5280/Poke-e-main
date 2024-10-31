6a040fc0baae0c5ac201dbcaf956c9db9681079c
dentij-etsu <111798237+dentij-etsu@users.noreply.github.com>
2024-10-31 15:42:59 -0400

    adding some code to simulate walking

--- Program.cs
+++ Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int x = 0, y = 0;
+        ConsoleKeyInfo cki;
+
+        string map = (  "----------\n" +
+                        "|        |\n" +
+                        "|        |\n" +
+                        "|        |\n" +
+                        "|        |\n" +
+                        "----------\n");
+
+        System.Console.WriteLine(map);
+
+        while (true)
+        {
+            cki = System.Console.ReadKey();
+            if (cki.KeyChar == 'w') y++;
+            if (cki.KeyChar == 'a') x--;
+            if (cki.KeyChar == 's') y--;
+            if (cki.KeyChar == 'd') x++;
+            System.Console.Clear();
+            System.Console.WriteLine(map);
+            System.Console.Write($"X: {x} Y: {y}");
+        }
     }
 }

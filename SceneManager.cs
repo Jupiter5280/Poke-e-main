9b1d8326b6747426b4f057f9adf59e205ba50293
dentij-etsu <111798237+dentij-etsu@users.noreply.github.com>
2024-11-06 09:42:59 -0500

    refactoring maps to static class and map logic to different class

---
+++ SceneManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poke_e_main
+{
+    internal class SceneManager
+    {
+
+        public SceneManager() { }
+
+        public void LoadScene(string[] map, int player_x, int player_y)
+        {
+            while (true)
+            {
+
+            }
+        }
+    }
+}
